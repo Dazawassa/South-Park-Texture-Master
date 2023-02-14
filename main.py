477eca2e851b4d7e38a3c99a184b446d97514f9e
Jacqueline <jackmcbeth28469@gmail.com>
2023-02-14 20:31:39 +0000

    Backbone for code.
    The back bone for the code is done. Now I need to make it read the file and write to files.

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+#Imported to create files and directories.
+import os
+
+#Function to open textures.
 def opentexturefile():
-    file_path = south_park_textures
-    file = open(file_path)
-    input("Textures found.")
+    global file
+    #Exists for error check.
+    file = "0"
+    file = open(south_park_textures)
+    if file != "0":
+        input("Textures found.")
+    create_directory()
 
+#Creates folder to rip to.
+def create_directory():
+    #Name of the folder.
+    folder_name = "Ripped Textures"
+    #If folder isn't present make it.
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        input(f"Folder '{folder_name}' created successfully.")
+    else:
+        #If it is present then don't.
+        input(f"Folder '{folder_name}' already exists.")
+    write_files()
 
+def write_files():
+    texture_list = 100
 
+#Defines location for texture file. Later this will be a user input.
+global south_park_textures
 south_park_textures = "win32.tex"
-global soth_park_textures
+#Runs function to open texture.
+opentexturefile()
