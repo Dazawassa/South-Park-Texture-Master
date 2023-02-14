ec3c8783ecbdc9dc361b52036c199ee87ff8e290
Jacqueline <jackmcbeth28469@gmail.com>
2023-02-14 19:25:18 +0000

    First commit
    Bones of the program to rip textures. Haven't tested just need a file here.

---
+++ main.py
@@ -0,0 +1,9 @@
+def opentexturefile():
+    file_path = south_park_textures
+    file = open(file_path)
+    input("Textures found.")
+
+
+
+south_park_textures = "win32.tex"
+global soth_park_textures
